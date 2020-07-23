7847dd6562852591b3699b959714bd83cf53f6ea
tamemotoyusuke <tamemotoyou@gmail.com>
2020-07-24 00:19:47 +0900

    update

--- python/2-2-1/hoge.py
+++ python/2-2-1/hoge.py
@@ -1,19 +1,16 @@
 import sys
 sys.setrecursionlimit(10 ** 5)
 
-# input_data = int(input())
-# otsuri = 1000 - input_data
-# res = []
-# yens = [500, 100, 50, 10, 5, 1]
+input_data = int(input())
+otsuri = 1000 - input_data
+res = []
+yens = [500, 100, 50, 10, 5, 1]
 
-# for i in yens:
-#     x = otsuri // i
-#     y = otsuri % i
-#     res.append(x)
-#     otsuri = y
+for i in yens:
+    x = otsuri // i
+    y = otsuri % i
+    res.append(x)
+    otsuri = y
     
-# print(sum(res))
-
-data = "ioioioi"
-print(data.count("ioi"))
+print(sum(res))
 
